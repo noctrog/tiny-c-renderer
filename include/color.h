a1d1a2c4689ed89bc25e935678fabec28cfa4cb6
Ramón Calvo <noctrog@gmail.com>
2021-01-22 11:24:02 +0100

    Color doxygen

--- include/color.h
+++ include/color.h
@@ -1,10 +1,18 @@
+/**
+ *   \file color.h
+ *   \brief Color related data structures.
+ */
+
+
 #ifndef COLOR_H_B3ZHITXW
 #define COLOR_H_B3ZHITXW
 
+/// Stores color represented as integers between 0-255 for each channel.
 struct coloru {
     unsigned char r, g, b;
 };
 
+/// Stores color represented as floating point values between 0.0-1.0 for each channel
 struct colorf {
     float r, g, b;
 };
