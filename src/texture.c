f1074dc35c14f7dbfa8d55d963880f5b6a91bd39
Ramón Calvo <noctrog@gmail.com>
2021-01-22 11:22:37 +0100

    Texture doxygen

--- src/texture.c
+++ src/texture.c
@@ -11,6 +11,14 @@ struct texture {
     float *color;           /* Color data */
 };
 
+/**
+ *  \brief Creates texture located in the heap from an image file. 
+ *
+ *  Needs to be freed afterwards.
+ *
+ *  \param filename: path to image file.
+ *  \return Pointer to the new texture.
+ */
 struct texture *
 rndr_texture_create(const char *filename)
 {
@@ -37,6 +45,11 @@ rndr_texture_create(const char *filename)
     return tex;
 }
 
+/**
+ *  \brief Deletes and frees a texture's memory
+ *  \param tex: pointer to a pointer to a texture.
+ *  \return void
+ */
 void
 rndr_texture_delete(struct texture **tex)
 {
@@ -46,6 +59,15 @@ rndr_texture_delete(struct texture **tex)
     *tex = NULL;
 }
 
+/**
+ *  \brief Returns the color of a given pixel
+ *
+ *  Returns NULL if pixel is invalid.
+ *
+ *  \param tex: pointer to a texture.
+ *  \param p: pointer to a vec2i indicating the pixel.
+ *  \return Pointer to a float color.
+ */
 struct colorf *
 rndr_texture_get_color(struct texture *tex, struct vec2i *p)
 {
@@ -57,6 +79,11 @@ rndr_texture_get_color(struct texture *tex, struct vec2i *p)
     return col;
 }
 
+/**
+ *  \brief Returns texture's width.
+ *  \param tex: pointer to a texture.
+ *  \return Texture width.
+ */
 int
 rndr_texture_get_width(struct texture *tex)
 {
@@ -64,6 +91,11 @@ rndr_texture_get_width(struct texture *tex)
     return tex->x;
 }
 
+/**
+ *  \brief Returns texture's height.
+ *  \param tex: pointer to a texture.
+ *  \return Texture height.
+ */
 int
 rndr_texture_get_height(struct texture *tex)
 {
